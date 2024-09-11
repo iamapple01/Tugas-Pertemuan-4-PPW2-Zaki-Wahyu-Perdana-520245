449bf445e5593e6f873b9be659d3987d5bda2d2a
Zaki Wahyu Perdana <zakiwahyuperdana@mail.ugm.ac.id>
2024-09-11 18:57:37 +0700

    initial commit

---
+++ resources/views/pesanan.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pesanan</title>
+</head>
+<body>
+   <h1>mas kopinya nggih !, kertasnya jangan lupa di print 100x nggih</h1> 
+</body>
+</html>
